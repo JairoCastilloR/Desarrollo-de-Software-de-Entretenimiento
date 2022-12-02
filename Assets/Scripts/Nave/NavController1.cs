c1ecff29cfb54d70d092c68db2d61a609c782700
José Miguel <jcanov@unsa.edu.pe>
2022-12-02 11:02:35 -0500

    Termiando implementacion escenario 1

---
+++ Assets/Scripts/Nave/NavController1.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavController1 : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    Transform transform;
+    [SerializeField]
+    GameObject dustCloud;
+
+    public float speed = 30f;
+    public float jumpSpeed = 20f;
+    public float distToGround = 0.5f;
+    bool IsGrounded = true;
+    private float curSpeed=0.0f;
+    private float maxSpeed = 30f;
+    public float acceleration=1.0f;
+    public float gravity = -9.8f;
+
+    AudioSource audioSource;
+
+    void Start()
+    {
+        
+        rigidbody = GetComponent<Rigidbody>();
+        transform = GetComponent<Transform>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+       
+        if (Input.GetKey(KeyCode.Space) && IsGrounded)
+        {
+            jumpSpeed += gravity * Time.deltaTime;
+            Vector3 jumpVelocity = new Vector3(0f,jumpSpeed,0f);
+            rigidbody.velocity += jumpVelocity;
+            gameObject.transform.Rotate(-8.5f, 0.0f, 0.0f);
+            IsGrounded = false;
+            audioSource.Play();
+        }
+       
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * Time.deltaTime * curSpeed;
+            curSpeed += acceleration * Time.deltaTime;
+            if (curSpeed > maxSpeed){ curSpeed = maxSpeed; }
+            
+        }else{
+            curSpeed -= acceleration * Time.deltaTime;
+            if (curSpeed < 0){ 
+                curSpeed = 0; 
+            }
+            transform.position += Vector3.forward * Time.deltaTime * curSpeed;
+            
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * Time.deltaTime * speed;
+        }
+      
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("platform")) {
+            //animation
+            Instantiate (dustCloud, transform.position,dustCloud.transform.rotation);
+            gameObject.transform.Rotate(0.0f, 0.0f, 0.0f);
+            audioSource.Stop();
+            IsGrounded = true;
+        }
+    }
+
+   
+}
